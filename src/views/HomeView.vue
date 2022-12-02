38e3e7974d6a32a8399027d0368abc3d6f8917d5
davidwah <davidwahyuyu@gmail.com>
2022-12-02 22:45:05 +0700

    add Header & Carousel

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <HeaderVue/>
+    <CarouselVue/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderVue from '@/components/Header.vue';
+import CarouselVue from '@/components/Carousel.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderVue,
+    CarouselVue,
+    
+  }
+}
+</script>

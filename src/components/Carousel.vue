7d9149925c22124023bb44d83bdefdc43807ec68
davidwah <davidwahyuyu@gmail.com>
2022-12-02 23:26:19 +0700

    add Items & Insta

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,54 +1,52 @@
 <template>
     <!-- Hero Section Begin -->
     <section class="hero-section">
-      <carousel class="hero-items" :items="1" :autoplaySpeed="2000" :loop="true" :mouseDrag="true" :nav="false" :wrap-around="true">
-  
-        <div class="single-hero-items set-bg" style="background-image: url('/img/hero-1.jpg')">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-               <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
+        <carousel class="hero-items" :items="1" :autoplaySpeed="2000" :loop="true" :mouseDrag="true" :nav="false"
+            :wrap-around="true">
+
+            <div class="single-hero-items set-bg" style="background-image: url('/img/hero-1.jpg')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <span>Bag,kids</span>
+                            <h1>Black friday</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore</p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="single-hero-items set-bg" style="background-image: url('/img/hero-2.jpg')">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-               <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
+            <div class="single-hero-items set-bg" style="background-image: url('/img/hero-2.jpg')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <span>Bag,kids</span>
+                            <h1>Black friday</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore</p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <!-- <template #addons>
+            <!-- <template #addons>
           <Navigation />
           <Pagination />
         </template> -->
-  
-      </carousel>
+
+        </carousel>
     </section>
     <!-- Hero Section End -->
-  </template>
+</template>
   
-  <script>
-  import carousel from "vue-owl-carousel";
-//   import 'vue3-carousel/dist/carousel.css'
-  // import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
-//   import { Carousel, Slide, Navigation, Pagination } from 'vue3-carousel'
-  
-  
-  
-  export default {
+<script>
+import carousel from "vue-owl-carousel";
+
+export default {
     name: "CarouselVue",
     components: {
-        carousel,      
+        carousel,
     },
-  }
-  </script>
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <HeaderVue/>
     <CarouselVue/>
+    <ItemsVue/>
+    <InstaVue/>
   </div>
 </template>
 
@@ -9,13 +11,17 @@
 // @ is an alias to /src
 import HeaderVue from '@/components/Header.vue';
 import CarouselVue from '@/components/Carousel.vue';
+import ItemsVue from '@/components/Items.vue';
+import InstaVue from '@/components/Insta.vue';
+
 
 export default {
   name: 'HomeView',
   components: {
     HeaderVue,
     CarouselVue,
-    
+    ItemsVue,
+    InstaVue
   }
 }
 </script>
